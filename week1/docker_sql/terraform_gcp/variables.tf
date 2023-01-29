4927f52c6196488c95422b8667f02777ee2d7dc4
Artur Khismatullin <zajurus@gmail.com>
2023-01-29 18:43:13 +0300

    add terraform gcp configuration

---
+++ week1/docker_sql/terraform_gcp/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "dtc_data_lake"
+}
+
+variable "project" {
+  default = "arkh-375916"
+  description = "dcde23"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "us-west1"
+  type = string
+}
+
+variable "storage_class" {
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "trips_data_all"
+}
